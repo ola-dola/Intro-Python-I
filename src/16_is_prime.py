06b3024f48de88915c8e8dd27b1053c758bca3df
ola-dola <oredolaolamide@gmail.com>
2020-06-03 13:31:39 +0100

    check primality using trial division

--- src/16_is_prime.py
+++ src/16_is_prime.py
@@ -1,13 +1,27 @@
 import sys
+import math
 
-def is_prime(number):
+
+# initial algorithm
+def is_prime_a(number):
     for x in range(2, number):
         if number % x == 0:
-            print(x)
+            # print(x)
+            return False
+    return True
+
+
+# check primality using trial division
+def is_prime_b(usr_input):
+
+    n = math.ceil(math.sqrt(usr_input))
+    print(n)
+    for x in range(2, n):
+        if n % x == 0:
             return False
     return True
 
 
 number = int(sys.argv[1])
 
-print(is_prime(number))
+print(is_prime_b(number))
